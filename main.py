a5e3d3f5a3b776d41d46bb4a82b33270401f4c7a
DuolanK <101317307+DuolanK@users.noreply.github.com>
2022-09-03 06:30:50 +0900

    windows only opencv scanning, 4 monitors number of iterations color recognition autorelise drones after damage

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ class POINT(Structure):
 def queryMousePosition():
     pt = POINT()
     windll.user32.GetCursorPos(byref(pt))
-    return {"x": pt.x, "y": pt.y}
+    return {"x": pt.x, "y": pta.y}
 
 
 def click():
@@ -75,18 +75,19 @@ def ReleaseKey(hexKeyCode):
     x = Input( ctypes.c_ulong(1), ii_ )
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
-# код мсс, захват картинки
+# код мсс, захват картинки #69 116 84, 49 186 104
 
 HSV_RANGES = {
-    'blue': [
+    'green': [
         {
-            'lower': np.array([101, 60, 45]),
-            'upper': np.array([250, 60, 64])
+            'lower': np.array([0, 0, 229]),
+            'upper': np.array([180, 38, 255])
         },
         {
-            'lower': np.array([101, 60, 64]),
-            'upper': np.array([250, 60, 64])
+            'lower': np.array([0, 0, 229]),
+            'upper': np.array([180, 38, 255])
         }
+
 ],  # orange is a minor colorцц
     'orange': [
         {
@@ -102,10 +103,10 @@ HSV_RANGES = {
         }
     ],
     # green is a major color
-    'green': [
+    'blue': [
         {
-            'lower': np.array([41, 39, 64]),
-            'upper': np.array([80, 255, 255])
+            'lower': np.array([101, 60, 45]),
+            'upper': np.array([250, 60, 64])
         }
     ],
     # cyan is a minor color
@@ -115,7 +116,7 @@ HSV_RANGES = {
             'upper': np.array([100, 255, 255])
         }
     ],
-    # blue is a major color
+    # blue is a major colorw
     'red': [
         {
             'lower': np.array([0, 50, 50]),
@@ -147,15 +148,18 @@ HSV_RANGES = {
     # white is all H values, lower 15% of S, & upper 10% of V
     'white': [
         {
-            'lower': np.array([0, 0, 229]),
-            'upper': np.array([180, 38, 255])
-        }
+            'lower': np.array([60, 39, 10]),
+            'upper': np.array([69, 116, 84])
+        },
 ]
 }
-monitor1 = {"top": 917, "left": 1756, "width": 40, "height": 40}
-monitor2 = {"top": 937, "left": 1733, "width": 40, "height": 40}
-monitor3 = {"top": 937, "left": 1781, "width": 40, "height": 40}
-monitor_drone = {"top": 918, "left": 1737, "width": 100, "height": 100}
+monitor1 = {"top": 875, "left": 1756, "width": 40, "height": 40}
+monitor2 = {"top": 900, "left": 1749, "width": 20, "height": 40}
+monitor3 = {"top": 900, "left": 1781, "width": 20, "height": 40}
+monitor4 = {"top": 860, "left": 1756, "width": 40, "height": 30}
+monitor_drone = {"top": 998, "left": 1350, "width": 115, "height": 10} #left 1320
+
+
 
 def process_image(original_image):
     # Создать копию входящей картинки в оттенках серого
@@ -196,7 +200,7 @@ def screen_record():
     sct = mss.mss()
     time.sleep(5)
 
-    N = 100000  # the number of iterationsq
+    N = 100000  # the number of iterations
 
     while True:
         # Проверить нижнюю область.
@@ -211,19 +215,24 @@ def screen_record():
             img = np.array(img)
             img = cv2.convertScaleAbs(img, alpha=alpha, beta=beta)
             img_hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-            red_mask = create_mask(img_hsv, ['red'])
+            red_mask = create_mask(img_hsv, ['green'])
             mask_img = cv2.bitwise_and(img_hsv, img_hsv, mask=red_mask)
             hasRed = np.sum(mask_img)
-            if hasRed > 200:
-                print('Blue', hasRed)
+            if hasRed < 91000:
+                print('DAMAGE', hasRed)
                 ReleaseKey(0x10)
                 PressKey(0x10)
-                time.sleep(random.uniform(1,2))
+            if hasRed == 0:
+                time.sleep(5)
+                pg.moveTo(1700, 800)  # {"top": 350, "left": 1360, "width": 100, "height": 50}
+                pg.click()
             else:
-                print('neblue', hasRed)
-                time.sleep(random.uniform(1,2))
+                print('notdamage', hasRed)
+                time.sleep(random.uniform(1, 2))
             cv2.imshow("OpenCV/Numpy", img)
-#ww
+
+
+            #wwцыфй
             img = sct.grab(monitor1)
             img = np.array(img)
             processed_image = process_image(img)
@@ -233,57 +242,100 @@ def screen_record():
             img3 = sct.grab(monitor3)
             img3 = np.array(img3)
             processed_image3 = process_image(img3)
+            img4 = sct.grab(monitor4)
+            img4 = np.array(img4)
+            processed_image4 = process_image(img4)
 
             # Посчитать среднее арифметическое всех границ. Если это значение отличается от 0,
             # то на нашей картинке есть препятствие. А значит его нужно перепрыгнуть.
-            mean1 = np.mean(processed_image)
+            mean1 = round(np.mean(processed_image),0)
             print('down mean1 = ', mean1) #frontw
             last_time = time.time()
-
-            mean2 = np.mean(processed_image2) #left
+            mean2 = round(np.mean(processed_image2),0) #left
             print('down mean2 = ', mean2)
             last_time = time.time()
-            mean3 = np.mean(processed_image3) #right
+            mean3 = round(np.mean(processed_image3),0) #rightwdwaq
             print('down mean3 = ', mean3)
             last_time = time.time()
+            mean4 = round(np.mean(processed_image4), 0)  # rightwdwaq
+            print('down mean4 = ', mean4)
+            last_time = time.time()
+
             mean1_df.append(mean1)
             mean2_df.append(mean2)
             mean3_df.append(mean3)
-            if mean1<float(6):
-                ReleaseKey(0x20) #D
-                ReleaseKey(0x1E) #A
-                PressKey(0x11) #W
-                time.sleep(random.uniform(0.1,0.2))
-            else:
-                if mean2>float(6):
-                    ReleaseKey(0x1E) #A
-                    PressKey(0x11)
-                    PressKey(0x20) #D
-                    time.sleep(random.uniform(0.05, 0.125))
-                if mean3>float(6):
-                    ReleaseKey(0x20)
-                    PressKey(0x11)
-                    PressKey(0x1E) #A
-                    time.sleep(random.uniform(0.05, 0.125))
-
-                if i >= 3:
+            koef_prepyatstsvie = 2
+            PressKey(0x11)
+            if mean1*koef_prepyatstsvie>mean4+mean1 or mean1<10:
+                PressKey(0x11)
+                ReleaseKey(0x1F)
+                ReleaseKey(0x20)
+                ReleaseKey(0x1E)
+                print('forward')
+                PressKey(0x13)  # R
+                if i >= 5:
                     if mean1 != 0 or mean2 != 0 or mean3 != 0:
-                        if mean1==mean1_df[i-1]==mean1_df[i-2] or mean2==mean2_df[i-1]==mean2_df[i-2] or mean3==mean3_df[i-1]==mean3_df[i-2]:
-                            ReleaseKey(0x1E)
-                            ReleaseKey(0x20)
+                        if mean1 == mean1_df[i - 2] == mean1_df[i - 3] and mean2 == mean2_df[i - 2] == mean2_df[
+                            i - 3] and mean3 == mean3_df[i - 2] == mean3_df[i - 3]:
+                            PressKey(0x13)  # R
                             ReleaseKey(0x11)
-                            PressKey(0x1F)  # S
-                            if i % 2 == 0:
-                                PressKey(0x20) #D
+                            PressKey(0x1F)  # Sa
+                            if mean3 > mean2:
+                                PressKey(0x20)  # D
+                                time.sleep(2)
+                                ReleaseKey(0x20)
                             else:
-                                PressKey(0x1E) #Awdwdwd
-                            time.sleep(0.5)
+                                PressKey(0x1E)  # A
+                                time.sleep(2)
+                                ReleaseKey(0x1E)  # A
+                            print('nazad')
+            elif (mean1+mean4)*koef_prepyatstsvie > mean2 and mean1+mean4 > mean3 and mean2 > mean3 and mean2>10:
+                PressKey(0x20)  #D
+                PressKey(0x11) #W
+                ReleaseKey(0x1E) #A
+                ReleaseKey(0x1F)
+                print('vpravo')
+                PressKey(0x13)  # R
 
 
-            print('loop took {} seconds'.format(time.time() - last_time))
-            last_time = time.time()
+            elif (mean1+mean4)*koef_prepyatstsvie > mean2 and mean1+mean4 > mean3 and mean3>mean2 and mean3>10:
+                PressKey(0x1E) #A
+                PressKey(0x11)
+                ReleaseKey(0x20) #D
+                ReleaseKey(0x1F)
+                print('vlevo')
+                PressKey(0x13)  # R
+
+
+            if i >= 1:
+                if mean1 != 0 or mean2 != 0 or mean3 != 0:
+                    if mean1 == mean1_df[i - 1] == mean1_df[i - 2] and mean2 == mean2_df[i - 1] == mean2_df[
+                        i - 2] and mean3 == mean3_df[i - 1] == mean3_df[i - 2]and mean1+mean4>0:
+                        PressKey(0x13)  # R
+                        ReleaseKey(0x11)
+                        PressKey(0x1F)  # Sa
+                        if mean3 > mean2:
+                            PressKey(0x20)  # D
+                            time.sleep(2)
+                            ReleaseKey(0x20)
+                        else:
+                            PressKey(0x1E)  # A
+                            time.sleep(2)
+                            ReleaseKey(0x1E)  # A
+                        print('nazad')
+
+                #if mean1==0 and mean2==0 and mean3==q0:
+                #    pg.moveTo(1700, 800)  # {"top": 350, "left": 1360, "width": 100, "height": 50}
+                #    pg.click()
+                print('loop took {} seconds'.format(time.time() - last_time))
+                last_time = time.time()
+            else:
+                PressKey(0x11)  # Wdw
+                time.sleep(random.uniform(0.05, 0.1))
+
+
 
 if __name__ == '__main__':
     screen_record()
 
-#
+#q
